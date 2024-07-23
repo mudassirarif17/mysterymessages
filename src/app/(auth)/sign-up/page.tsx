6670301823e0f8ccb6433f0e540c8ea5af223f21
Mudassir <mudassirinoxent@gmail.com>
2024-07-23 23:15:52 +0500

    sign-up-page is half completed

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+"use client"
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
 
 const page = () => {
+  const [username , setUsername] = useState('');
+  const [usernamemessage , setusernameMessage] = useState('');
+  const [isCheckingUsername , setIsCheckingUsername] = useState(false);
+  const [isSubmitting , setIsSubmitting] = useState(false);
+
   return (
     <div className='sign-up md:flex justify-center bg-gray-100 '>
         <div className='sign-up-child w-[100vw] md:w-[40vw] bg-white shadow-xl'>
