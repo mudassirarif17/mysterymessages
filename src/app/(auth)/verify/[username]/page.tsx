55ce4b5648c796e9686d760c845fa21d65480f55
Mudassir <mudassirinoxent@gmail.com>
2024-07-28 19:18:35 +0500

    verify code page ui is ready with some bugs

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
 import { useToast } from '@/components/ui/use-toast';
 import { verifySchema } from '@/schemas/verifySchema';
 import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios, { AxiosError } from 'axios';
 import { useParams , useRouter } from 'next/navigation';
+import { Input } from "@/components/ui/input";
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import * as z from "zod";
@@ -47,8 +49,36 @@ const VerifyUsername = () => {
 
 
   return (
-    <div>
-      
+    <div className='sign-up md:flex justify-center items-center h-[100vh] bg-gray-100 '>
+
+        <div className='sign-up-child w-[100vw] h-[56vh] my-4 md:w-[32vw] bg-white rounded-md shadow-xl'>
+
+            <div className="description my-4">
+                <h1 className='text-center text-5xl font-extrabold'>Verify Your<br />Account</h1>
+                <p className='text-center text-md my-2'>Enter the verification code sent to your email</p>
+            </div>
+
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto space-y-6 w-5/6">
+                <FormField
+                control={form.control}
+                name="code"
+                render={({field})=>(
+                  <FormItem>
+                    <FormLabel>Verify Code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="verify code"
+                      {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )} 
+                />
+                <button className="bg-black text-white px-4 rounded-lg py-1" type="submit">Submit</button>
+              </form>
+            </Form>
+        </div>
+
     </div>
   )
 }
